8a21b303e5e0f9acad71805f731acd07d73cc97f
Zajuli01 <zajulitaupiqbisri@gmail.com>
2024-01-02 14:22:23 +0700

    Technical

--- ViewModels/RegisterVM.cs
+++ ViewModels/RegisterVM.cs
@@ -4,19 +4,14 @@ namespace CustomIdentity.ViewModels
 {
     public class RegisterVM
     {
-        [Required]
         public string? Name { get; set; }
-        [Required]
         [DataType(DataType.EmailAddress)]
         public string? Email { get; set; }
-        [Required]
+        public int? PhoneNumber { get; set; }
         [DataType(DataType.Password)]
         public string? Password { get; set; }
 
-        [Compare("Password", ErrorMessage = "Passwords don't match.")]
-        [Display(Name = "Confirm Password")]
-        [DataType(DataType.Password)]
-        public string? ConfirmPassword { get; set; }
+        public string? UploadFoto { get; set; }
 
     }
 }
